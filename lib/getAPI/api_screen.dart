520b02c7b22f88e903e16effd1d2f9147b88a885
Usman-bin-Khalid <usmanbinkhalidpk@gmail.com>
2026-01-02 10:30:00 +0500

    API Screen Completed

---
+++ lib/getAPI/api_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ApiScreen extends ConsumerWidget {
+  const ApiScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(),
+      floatingActionButton: FloatingActionButton(onPressed: () {}),
+    );
+  }
+}
+
+class PostModel {
+  final int id;
+  final int userId;
+  final String title;
+  final String body;
+
+  PostModel({required this.id, required this.userId, required this.title, required this.body});
+  
+}
