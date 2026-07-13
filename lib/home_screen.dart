bc0da5c3cfa9bd59ccc2213d359914578f855b48
Usman-bin-Khalid <usmanbinkhalidpk@gmail.com>
2026-07-13 22:48:09 +0500

    Consumer Widget Add

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,18 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
 final hello = Provider<String>((ref) {
-  return "Hello, Riverpod!";
+  return "Subscribe";
+});
+final age = Provider<int>((ref) {
+  return 20;
 });
 
-class HomeScreen extends StatelessWidget {
+final counter = StateProvider<int>((ref) {
+  return 0;
+});
+
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final count = ref.watch(counter);
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Screen')),
-      body: const Center(child: Text('Welcome to the Home Screen!')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(count.toString()),
+            Row(
+              children: [
+                ElevatedButton(
+                  child: Text('+'),
+                  onPressed: () {
+                    ref.read(counter.notifier).state++;
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+
+// class HomeScreen extends ConsumerWidget {
+//   const HomeScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final subscribe = ref.watch(hello);
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Home Screen')),
+//       body: Center(child: Text(subscribe + age.toString())),
+//     );
+//   }
+// }
+
