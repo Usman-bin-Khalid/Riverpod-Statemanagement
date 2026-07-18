6497fa44c986abcac9b89784630308d8e5232777
Usman-bin-Khalid <usmanbinkhalidpk@gmail.com>
2026-07-18 20:10:58 +0500

    Add Item Method Created

--- lib/provider/favourite_provider.dart
+++ lib/provider/favourite_provider.dart
@@ -1,12 +1,26 @@
 import 'package:riverpod/legacy.dart';
+import 'package:riverpod_state_management/models/item.dart';
+
 import 'package:riverpod_state_management/provider/favourite_state.dart';
 
-final favouriteProvider = StateNotifierProvider<FavouriteNotifier, FavouriteState>((ref) {
-  return FavouriteNotifier();
-}); 
-
-
+final favouriteProvider =
+    StateNotifierProvider<FavouriteNotifier, FavouriteState>((ref) {
+      return FavouriteNotifier();
+    });
 
 class FavouriteNotifier extends StateNotifier<FavouriteState> {
-  FavouriteNotifier () : super(FavouriteState(allItems: [], filteredItem: [], search: ''));
+  FavouriteNotifier()
+    : super(FavouriteState(allItems: [], filteredItem: [], search: ''));
+
+  void addItem() {
+    List<Item> items = [
+      Item(name: "Macbook", id: 1, favourite: true),
+      Item(name: "Iphoe", id: 2, favourite: false),
+      Item(name: "G-Force", id: 3, favourite: false),
+      Item(name: "Samsung Ultra", id: 4, favourite: true),
+      Item(name: "Google Pixel 9", id: 5, favourite: false),
+      Item(name: "IPad Pro 13", id: 6, favourite: true),
+    ];
+    state = state.copyWith(allItems: items.toList(), filteredItem: items.toList());
+  }
 }
